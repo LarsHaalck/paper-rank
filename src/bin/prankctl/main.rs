e0de34d31ec17c1afea5a8e3d398300efb6d74eb
Lars Haalck <lars.haalck@uni-muenster.de>
2021-07-31 14:56:25 +0200

    Add prankctl stub

--- src/bin/prankctl/main.rs
+++ src/bin/prankctl/main.rs
@@ -1,5 +1,34 @@
-use prank::user::User;
-fn main()
-{
-    println!("hi");
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(about = "prankctl: CLI tool to manage prank")]
+enum PrankCtl {
+    Users(UsersSubcommand),
+    Items(ItemsSubcommand)
+}
+
+#[derive(StructOpt, Debug)]
+enum UsersSubcommand {
+    Approve(Options),
+    Reject(Options),
+    Show(Options)
+}
+
+#[derive(StructOpt, Debug)]
+enum ItemsSubcommand {
+    Delete(Options),
+    Show(Options)
+}
+
+#[derive(StructOpt, Debug)]
+struct Options {
+    #[structopt(long)]
+    all: bool,
+    #[structopt(conflicts_with = "all", required_unless = "all")]
+    ids: Vec<u32>
+}
+
+fn main() {
+    let opt = PrankCtl::from_args();
+    println!("{:#?}", opt);
 }
